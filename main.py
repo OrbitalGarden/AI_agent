a3e21b20c940a1fb2c156a0cbe36612b17f3ad76
OrbitalGarden <orbitalgarden@outlook.com>
2025-10-14 15:03:49 +0200

    add user input and --verbose. Prepare for prompt history.

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import os
+import sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if len(args) < 1:
+        print("AI agent usage: python3 main.py 'prompt' [--verbose]")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    verbose = True if "--verbose" in sys.argv else False
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."]
+        contents=messages
     )
     print(response.text)
-    #print(Prompt tokens: X)
-    #print(Response tokens: Y)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
